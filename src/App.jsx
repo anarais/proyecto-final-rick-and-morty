84fe61473511a7cc9071a4f6ac3f410153ba95e1
anarais <thiagokozur@hotmail.com>
2025-06-24 20:23:02 -0300

    cambios para subir a pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
